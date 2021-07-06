3c8697b4a6ba025817ce36c15b5a685dd00460a6
Ashkan Ashtiani <ashcan@3dln.com>
2021-07-06 16:25:41 +0430

    validate directory existence and package.json existence

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,24 @@
 
 import Commander from 'commander'
 import chalk from 'chalk'
-import prompts from 'prompts'
+import prompts, { Answers } from 'prompts'
+import { existsSync } from 'fs'
+import { basename, resolve } from 'path'
 
-import packageJson from '../package.json'
+// dployr package.json file
+import dployrPackageJson from '../package.json'
 
-// initialize the project path with an empty string
+// Initialize the project path with an empty string
 let projectPath: string = ''
+// Project name
+let projectName: string = ''
+// Absolute path to the nodejs project
+let resolvedProjectPath: string = ''
 
 // This is the main program
-const program = new Commander.Command(packageJson.name)
+const program = new Commander.Command(dployrPackageJson.name)
   // Assign npm version to the program
-  .version(packageJson.version)
+  .version(dployrPackageJson.version)
   .usage(
     `${chalk.green(
       'Provide the project path with -P or --path option'
@@ -25,36 +32,42 @@ const program = new Commander.Command(packageJson.name)
 // Parses the passes options and arguments
 program.parse(process.argv)
 
-// This is the main function and runs right after excecution of the program
+// Asks for project path
+async function askProjectPath(): Promise<string> {
+  const res = await prompts({
+    type: 'text',
+    name: 'path',
+    message:
+      'Path to the Node.js project?',
+    initial: './',
+    validate: (path) => {
+      // TODO: Let's check if the path is valid
+      // a valid package.json file should be presented
+      // return the value based on the validation result
+      return true
+    },
+  })
+
+  return res.path
+}
+
+// This is the main function and runs right after execution of the program
 async function run(): Promise<void> {
   const options = program.opts()
   // Check if user provided a path to the project directory
   if (options.path) {
     // User did specified a path
     if (typeof options.path === 'string') {
-      projectPath = options.path.trim()
-      // TODO: validate the specified path
+      projectPath = options.path
+      onProjectPath()
     }
   } else {
     // User didn't specified a path
     // Lets explicitly ask for a path
-    const resp = await prompts({
-      type: 'text',
-      name: 'path',
-      message:
-        'Path to the Node.js project?',
-      initial: './',
-      validate: (path) => {
-        // TODO: Let's check if the path is valid
-        // a valid package.json file should be presented
-        // return the value based on the validastion result
-        return true
-      },
-    })
-
-    if (typeof resp.path === 'string') projectPath = resp.path.trim()
+    getProjectPath()
   }
 
+  // TODO: is this condition ever going to happen?
   if (!projectPath) {
     // we steel don't have a project path
     // TODO: Ask for the project path with more detailed guidance
@@ -62,6 +75,75 @@ async function run(): Promise<void> {
   } else {
     // Project path is specified and validated
     // TODO: Let's go for next steps
+  }
+}
+
+function getProjectPath(retry = false) {
+  // if retry specified we want to provide a verbose guide
+  if (retry) {
+    console.log(`${chalk.yellowBright(`
+Please provide a proper path to a nodejs project root
+(../projects/my-nodejs-project)
+`)}`)
+  }
+  askProjectPath().then(
+    path => {
+      if (typeof path === 'string') {
+        projectPath = path
+        onProjectPath()
+      }
+    }
+  )
+}
+
+function onProjectPath() {
+  projectPath = projectPath.trim()
+  resolvedProjectPath = resolve(projectPath)
+  const pathExists = validatePathOrFile(resolvedProjectPath)
+  // Provided path exists
+  console.log()
+  console.log(`Validating specified path... ${pathExists ? chalk.green('ok') : chalk.red('failed')}`)
+
+  if (!pathExists) {
+    // Path validation failed
+    // Let's ask again with a verbose guide
+    getProjectPath(true)
+  } else {
+    // Path validation success
+    onPathValidationSuccess()
+  }
+  // TODO: moved this to appropriate place
+  // projectName = basename(resolvedProjectPath)
+
+}
+
+
+// Path validation success
+function onPathValidationSuccess() {
+  // Let's verify the project structure by locating a package.json file
+  // TODO: in project path fetching we should tell user to specify the root folder where package.json file exists in
+  const resolvedPackageJson = resolve(resolvedProjectPath, 'package.json')
+  const isPackageJson = validatePathOrFile(resolvedPackageJson)
+  console.log(`Check if package.json file exists... ${isPackageJson ? chalk.green('ok') : chalk.red('failed')}`)
+  if (!isPackageJson) {
+    console.log()
+    console.log(`Warning: ${chalk.yellow('Any nodejs project must have a package.json file')}`)
+    getProjectPath(true)
+  }
+
+}
+
+// Validate project path
+function validatePathOrFile(path: string): boolean {
+  try {
+    if (existsSync(path)) {
+      return true
+    } else {
+      return false
+    }
+  } catch (e) {
+    console.log("An error occurred.")
+    process.exit(1)
   }
 }
 
